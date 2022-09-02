219ede0869f5ab8570090f75c8abcc0d5d725d60
Syed Rahim Saqib <jackneutron786@gmail.com>
2022-09-02 11:47:29 +0530

    interfaces

--- JavaDemos/src/com/tnsif/lessonBasic_Java_Program/Code_Assignment.java
+++ JavaDemos/src/com/tnsif/lessonBasic_Java_Program/Code_Assignment.java
@@ -1,15 +1,29 @@
 package com.tnsif.lessonBasic_Java_Program;
 
 public class Code_Assignment {
+	int Hello = 1;
+	
+	public void hello() {
+		System.out.println(this.Hello);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i=0; i<5; i++) {
-			for (int j=0; j<=i; j++) {
-				System.out.print("@");
-			}
-			System.out.println("");
-		}
+//		for (int i=0; i<5; i++) {
+//			for (int j=0; j<=i; j++) {
+//				System.out.print("@");
+//			}
+//			System.out.println("");
+//		}
+		Code_Assignment h = new Code_Assignment();
+		h.hello();
+//		Integer arr[] = new Integer[20];
+//		arr[0] = 1;
+//		System.out.println(arr);
+//
+//		for (Integer ele : arr) {
+//			System.out.println(ele);
+//		}
 	}
 
 }
